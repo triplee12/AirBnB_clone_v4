5e41d9e2c32408977cb67295945e8156f5583bf7
EBUKA EMMANUEL EJIE <tripleeoliver@gmail.com>
2023-05-06 14:59:08 -0700

    Web dynamic

---
+++ api/v1/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" AirBnB api v1 """
+from models import storage
+from api.v1.views import app_views
+from os import environ
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
